143167c2c5fae308a7dcdb08b2360ebb1571b67a
Cracklitos7 <160290197+Cracklitos7@users.noreply.github.com>
2024-05-11 04:55:43 +0000

    Proyecto Stickman

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/dom/elements.hpp>
+#include <thread>
+#include <fstream>
+#include <string>
+#include <Dibujo.hpp>
+
+using namespace std;
+using namespace ftxui;
+
+int main(int argc, char const *argv[])
+{
+  Dibujo pistola("assets/images/pistola.txt");
+
+  int fotograma = 0;
+  while (true)
+  {
+    this_thread::sleep_for(0.1s);
+    fotograma++;
+    Element personaje = spinner(21, fotograma) | bold | color(Color::Red) | bgcolor(Color::White);
+    Element dibujo = hbox(personaje, moto.GetElement(), bloque.GetElement());
+    Screen pantalla = Screen::Create(Dimension::Full());
+    Render(pantalla, dibujo);
+    pantalla.Print();
+    cout << pantalla.ResetPosition();
+  }
+
+  return EXIT_SUCCESS;
+}
